3f9536d168be3e82bf3fc1b28f292fd146b21542
SketchPiece <lubkin.andrey@gmail.com>
2023-10-10 13:51:23 -0700

    wip: add temp macos foreground processes script

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,23 +1,39 @@
 use sysinfo::{PidExt, ProcessExt, System, SystemExt};
+mod utils;
 
 #[derive(serde::Serialize, Debug)]
 pub struct AppProcess {
     id: u32,
     name: String,
+    foreground: bool,
 }
 
 pub fn get_system_processes() -> Vec<AppProcess> {
-    let mut processes: Vec<AppProcess> = vec![];
+    // let mut processes: Vec<AppProcess> = vec![];
 
     let mut sys = System::new_all();
     sys.refresh_all();
 
-    for (pid, process) in sys.processes() {
-        processes.push(AppProcess {
-            id: pid.as_u32(),
-            name: process.name().to_owned(),
+    let foreground_processes_names = utils::foreground_processes::get_os_foreground_process_names();
+
+    let processes: Vec<AppProcess> = sys
+        .processes()
+        .iter()
+        .map(|(pid, process)| {
+            let process_name = process.name().to_string();
+            AppProcess {
+                id: pid.as_u32(),
+                foreground: foreground_processes_names.contains(&process_name),
+                name: process_name,
+            }
         })
-    }
+        .collect();
+    let debug_processes: Vec<&AppProcess> = processes
+        .iter()
+        .filter(|process| process.name == "Arc")
+        .collect();
+
+    println!("{debug_processes:#?}");
 
     processes
 }
